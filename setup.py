502ec6d10a235af9d786d4d03290626df0091d18
Uwe Fladrich <uwe.fladrich@smhi.se>
2021-03-19 14:29:59 +0100

    Fix #3: Rename package and modules
    - Rename package to 'sriptengine-hpc'
    - Rename SLURM module to 'scriptengine_hpc.slurm'

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ import os
 import codecs
 import setuptools
 
+
 def read(rel_path):
     """
     Helper function to read file in relative path.
@@ -10,6 +11,7 @@ def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
     with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
 
 def get_version(rel_path):
     """
@@ -20,18 +22,17 @@ def get_version(rel_path):
             delim = '"' if '"' in line else "'"
             return line.split(delim)[1]
 
+
 setuptools.setup(
-    name='scriptengine-tasks-hpc',
-    version=get_version('scriptengine/tasks/hpc/version.py'),
+    name='scriptengine-hpc',
+    version=get_version('scriptengine_hpc/version.py'),
     author='Uwe Fladrich',
     author_email='uwe.fladrich@protonmail.com',
     description='ScriptEngine tasks you may need on HPC systems',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://github.com/uwefladrich/scriptengine-tasks-hpc',
-    packages=[
-        'scriptengine.tasks.hpc',
-    ],
+    packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
@@ -43,7 +44,7 @@ setuptools.setup(
     ],
     entry_points={
         'scriptengine.tasks': [
-            'hpc.slurm.sbatch = scriptengine.tasks.hpc.slurm:Sbatch',
+            'hpc.slurm.sbatch = scriptengine_hpc.slurm:Sbatch',
         ],
     },
 )
