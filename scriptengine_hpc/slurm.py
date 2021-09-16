f7cadd4565fb5b83f1cb223b4b7b2799b489ccec
Uwe Fladrich <uwe.fladrich@smhi.se>
2021-09-16 11:45:56 +0200

    Fix #5: Support SLURM heterogeneous jobs

--- scriptengine_hpc/slurm.py
+++ scriptengine_hpc/slurm.py
@@ -9,11 +9,7 @@ from scriptengine.exceptions import ScriptEngineStopException, \
 
 
 _JOB_VAR = 'SLURM_JOB_NAME'
-_SUBMIT_CMD = 'sbatch'
-
-
-def is_batch_job():
-    return os.environ.get(_JOB_VAR) is not None
+_SBATCH_CMD = 'sbatch'
 
 
 class Sbatch(Task):
@@ -21,41 +17,72 @@ class Sbatch(Task):
     @timed_runner
     def run(self, context):
 
-        # No recursive job submission by default: check if we're already in a job
-        if is_batch_job() and not self.getarg('submit_from_batch_job', default=False):
+        def in_batch_job():
+            return os.environ.get(_JOB_VAR) is not None
+
+        def do_recursive_submit():
+            return self.getarg(
+                'submit_from_batch_job',
+                context,
+                default=False,
+            )
+
+        def is_sbatch_opt(opt):
+            return opt not in (
+                'scripts',
+                'submit_from_batch_job',
+                'hetjob_spec',
+            ) and not opt.startswith('_')
+
+        if in_batch_job() and not do_recursive_submit():
             self.log_debug(
-                f'{self.__class__.__name__} task from within a batch '
-                'job and "submit_from_batch_job" is not set or false: '
+                'Already running within a batch job and '
+                '"submit_from_batch_job" is not set or false - '
                 'not submitting new job'
             )
             return
 
-        # Append all opts,args that should go to the submit command to a list
-        opt_args = []
-        for opt, arg in self.__dict__.items():
-            if opt in ('scripts', 'submit_from_batch_job') or opt.startswith('_'):
-                continue
-            opt_args.append(f'--{opt}')
-            if arg:
-                opt_args.append(j2render(arg, context))
+        # Start building the sbatch command line
+        sbatch_cmd_line = [_SBATCH_CMD]
 
-        # Build the submit command line
-        submit_cmd = [_SUBMIT_CMD]
-        submit_cmd.extend(map(str, opt_args))
-        scripts = self.getarg('scripts', default=None)
+        sbatch_general_args = []
+        for opt, arg in self.__dict__.items():
+            if is_sbatch_opt(opt):
+                sbatch_general_args.append(f'--{opt}')
+                if arg:
+                    sbatch_general_args.append(j2render(arg, context))
+        sbatch_cmd_line.extend(map(str, sbatch_general_args))
+
+        hetjob_spec = self.getarg('hetjob_spec', context, default=[])
+        if hetjob_spec:
+            self.log_debug('Submitting a hetereogeneous SLURM job with SBATCH')
+            sbatch_hetjob_args = []
+            for job_args in hetjob_spec:
+                for opt, arg in job_args.items():
+                    if len(sbatch_hetjob_args) > 0:
+                        sbatch_hetjob_args.append(':')
+                    sbatch_hetjob_args.append(f'--{opt}')
+                    if arg:
+                        sbatch_hetjob_args.append(j2render(arg, context))
+            sbatch_cmd_line.extend(map(str, sbatch_hetjob_args))
+
+        scripts = self.getarg('scripts', context, default=None)
         if scripts:
-            submit_cmd.append('se')
-            submit_cmd.extend(
-                map(str, scripts if isinstance(scripts, list) else [scripts])
+            sbatch_cmd_line.append('se')
+            sbatch_cmd_line.extend(
+                map(
+                    str,
+                    scripts if isinstance(scripts, list) else [scripts],
+                )
             )
         else:
             # If no scripts were given, use the original SE command line
-            submit_cmd.extend(sys.argv)
-        self.log_debug(f'Command line for submitting job: {submit_cmd}')
+            sbatch_cmd_line.extend(sys.argv)
+        self.log_debug(f'Command line for submitting job: {sbatch_cmd_line}')
 
-        # Run submit command, with handling of errors
+        # Run sbatch command, with handling of errors
         try:
-            subprocess.run(submit_cmd, check=True)
+            subprocess.run(sbatch_cmd_line, check=True)
         except subprocess.CalledProcessError as e:
             self.log_error(
                 f'Submit command returns error: {e}'
