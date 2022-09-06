50034921312c65e8fd23323045bd095b41e9ae7a
Uwe Fladrich <uwe.fladrich@smhi.se>
2022-09-06 08:36:35 +0200

    Remove test file

--- testmod.py
+++
@@ -1,5 +0,0 @@
-from scriptengine_hpc.module import modfunc_from_lmod, modfunc_from_environment_modules
-
-module = modfunc_from_lmod("/home/sm_uflad/Projects/lmod") or modfunc_from_environment_modules()
-
-module("list")
