41d1ae1d1f3ab4b6634b5a8b87f1df2f9d3b7fc8
Uwe Fladrich <uwe.fladrich@smhi.se>
2024-01-24 14:27:50 +0100

    Update required Python version to 3.8

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setuptools.setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         'scriptengine>=0.8.0',
     ],
